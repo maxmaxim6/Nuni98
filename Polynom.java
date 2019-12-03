e919d9d1d1efd76f4412e55e647569e471e85cda
maxmaxim6 <maxmaxim7566@gmail.com>
2019-12-03 13:35:39 +0200

    Update Polynom.java

--- Polynom.java
+++ Polynom.java
@@ -1,4 +1,4 @@
-//*****************************THIS CODE CREATED BY Nuni98***********************************
+//****************************THIS CODE CREATED BY Nuni98***********************************
 package myMath;
 
 import java.util.ArrayList;
